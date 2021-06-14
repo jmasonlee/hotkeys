f640757d2e486a731c0d0f6935794628b5cde1e7
jmasonlee <jmasonlee@gmail.com>
2021-06-14 06:25:02 -0600

    r  autoformat

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -103,14 +103,14 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: "HelloWorld",
-  props: {
-    msg: String,
-  },
-});
+    name: 'HelloWorld',
+    props: {
+        msg: String,
+    },
+})
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -129,4 +129,5 @@ li {
 a {
   color: #42b983;
 }
+
 </style>
