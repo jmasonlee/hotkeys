11e9c969247af4a369288ad89d8b0e8ea1e77877
jmasonlee <jmasonlee@gmail.com>
2021-06-14 21:31:30 -0600

    B   export component

--- src/components/World.vue
+++ src/components/World.vue
@@ -3,6 +3,14 @@
 </template>
 
 <script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'World',
+    props: {
+        msg: String,
+    },
+})
 
 </script>
 
