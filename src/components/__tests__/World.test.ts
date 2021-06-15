a5f01f18d87ea5ef8f6fd031bec7cc650bc606c4
jmasonlee <jmasonlee@gmail.com>
2021-06-14 21:11:04 -0600

    R rename

--- src/components/__tests__/World.test.ts
+++ src/components/__tests__/World.test.ts
@@ -1,7 +1,7 @@
 import  { mount } from '@vue/test-utils'
 import World from '@/components/World.vue'
 
-describe('HelloWorld', () => {
+describe('World', () => {
     it('will greet people', function () {
         const msg = 'new message'
         const wrapper = mount(World, { props: { msg }})
